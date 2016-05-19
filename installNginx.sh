1e3b5587f4df0f1fddb70cabd5863ef7dbf52097
Dominic Ruettimann <doxic@users.noreply.github.com>
2016-05-19 21:24:03 +0200

    fixed wrong file ending

--- installNginx.sh
+++ installNginx.sh
@@ -76,7 +76,7 @@ make install || { echo "Installation failed"; exit 1; }
 id -u nginx &>/dev/null && echo "User exists" || useradd -r nginx
 
 # download init.d script and make executable
-curl -o /etc/init.d/nginx https://gist.githubusercontent.com/doxic/4917727d7f9558981646f783c9b5b043/raw/94c24d0f2d69d035e7195564961a19e38535676b/etc-init.d-nginx || { echo "init.d Script ${red}not downloaded${reset}" }
+curl -o /etc/init.d/nginx https://gist.githubusercontent.com/doxic/4917727d7f9558981646f783c9b5b043/raw/94c24d0f2d69d035e7195564961a19e38535676b/etc-init.d-nginx || { echo "init.d Script ${red}not downloaded${reset}"; }
 chmod +x /etc/init.d/nginx
 
 # Add service and set runlevel
